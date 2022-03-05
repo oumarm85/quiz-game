2ce7e40acbf5eb2c81087c0b24f4969bafa2a6da
oumarm85 <oumarmasood@gmail.com>
2022-03-05 19:49:57 +0000

    Initial Commit

---
+++ data.py
@@ -0,0 +1,41 @@
+question_data = [{"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                  "question": "English new wave musician Gary Numan founded the video game development company "
+                              "Facepunch Studios in March 2009.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Books",
+                  "type": "boolean", "difficulty": "easy",
+                  "question": "The book 1984 was published in 1949.",
+                  "correct_answer": "True",
+                  "incorrect_answers": ["False"]
+                  },
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "In the original script of &quot;The Matrix&quot;, the machines used humans as "
+                              "additional computing power instead of batteries.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean",
+                  "difficulty": "easy",
+                  "question": "In Kill La Kill, the weapon of the main protagonist is a katana. ",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                  "question": "Bulls are attracted to the color red.", "correct_answer": "False",
+                  "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                  "question": "In &quot;Super Mario 3D World&quot;, the Double Cherry power-up originated from a"
+                              " developer accidentally making two characters controllable.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "United States President John F. Kennedy was assassinated during his presidential "
+                              "motorcade in Atlanta, Georgia on November 22nd, 1963.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean",
+                  "difficulty": "easy",
+                  "question": "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the "
+                              "nickname Kyon.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Geography", "type": "boolean", "difficulty": "easy",
+                  "question": "St. Louis is the capital of the US State Missouri.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                  "question": "In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy the "
+                              "supernatural area known as  &quot;the Zone&quot;.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]}]
